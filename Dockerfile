c972d9ed8d71cbdd1463e4aa11920344f996d72c
Andy Shaw <ashaw@thoughtworks.com>
2015-06-28 00:48:17 +1000

    New gulfile.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM node:0.12.5-slim
-COPY package.json /app/package.json
+COPY package.json game.js /app/package.json
 COPY game /app/game
-COPY dist/js /app/game/js/gen
-COPY dist/css /app/game/css
 COPY supporting-libs /app/supporting-libs
 RUN npm i ensemblejs -g && cd /app && npm i && rm /bin/sh && ln -s /bin/bash /bin/sh
 EXPOSE  3000
--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,21 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var del = require('del');
 var flatten = require('gulp-flatten');
 var browserify = require('browserify');
 var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var transform = require('vinyl-transform');
-var exec = require('child_process').exec;
 
 var paths = {
   js: ['game/**/*.js', '!game/js/gen/**'],
-  scss: ['game/**/*.scss', 'plugins/**/src/scss/*.scss'],
-  css: ['game/css', 'plugins/**/public/*.css'],
+  scss: ['game/**/*.scss'],
+  css: ['game/css'],
   tests: ['tests/**/*.js'],
   genjs: './game/js/gen',
-  modes: ['./game/js/*.js', '!./game/js/modes.js']
+  modes: ['./game/js/*.js']
 };
 
 var onError = function (error) {
@@ -38,10 +35,9 @@ gulp.task('delete-gen-code', function (cb) {
 });
 gulp.task('clean', ['delete-gen-css', 'delete-gen-code']);
 
-gulp.task('test', ['clean'], function (cb) {
+gulp.task('test', ['clean'], function () {
     gulp.src(paths.tests)
-        .pipe(mocha({reporter: 'spec'}))
-        .on('end', cb);
+        .pipe(mocha({reporter: 'spec'}));
 });
 
 gulp.task('build-code', function() {
@@ -50,47 +46,30 @@ gulp.task('build-code', function() {
         return b.bundle();
     });
 
-    return gulp.src(paths.modes)
-        .pipe(plumber({errorHandler: onError}))
-        .pipe(browserified)
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.genjs));
-});
-gulp.task('build-code-fast', function() {
-    var browserified = transform(function(filename) {
-        var b = browserify(filename);
-        return b.bundle();
+    var fs = require('fs');
+    fs.mkdir('game/js/gen', function() {
+        fs.writeFileSync('game/js/gen/common.min.js', '');
     });
 
     return gulp.src(paths.modes)
         .pipe(plumber({errorHandler: onError}))
         .pipe(browserified)
+        .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(paths.genjs));
 });
+
 gulp.task('build-styles', function() {
     return gulp.src(paths.scss)
         .pipe(plumber({errorHandler: onError}))
         .pipe(autoprefixer({ cascade: false }))
         .pipe(sass({ style: 'expanded', sourcemapPath: 'public/css', bundleExec: true }))
         .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
         .pipe(flatten())
         .pipe(gulp.dest('game/css'));
 });
 gulp.task('build', ['build-styles', 'build-code']);
-gulp.task('build-fast', ['build-styles', 'build-code-fast']);
-
-gulp.task('start-server', function (cb) {
-    exec('start ' + process.cwd() + '/game', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-});
 
 gulp.task('default', ['test', 'build']);
-gulp.task('local', ['build-fast', 'start-server']);
+gulp.task('quick', ['clean', 'build']);
