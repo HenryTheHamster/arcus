7f58a18d3a70e05a25e41dd770bee6803d8b2890
andy shaw <shawry@shawry.com>
2015-10-19 17:16:23 +1100

    WIP

--- game/js/game.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var entryPoint = require('ensemblejs-client');
-entryPoint.loadWindow(require('window'));
-entryPoint.loadDefaults();
-entryPoint.load(require('./views/arcus'));
-entryPoint.run();
